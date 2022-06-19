196076e5918c165391369b8d6888a1cf3722bddc
Luis Miguel Barthaburu <barthaburuluismiguel@outlook.com>
2022-06-19 12:04:07 -0300

    funcion alta de areas

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const indexRouter = require('./routes/index');
+const areasRouter = require('./routes/areas');
 
 const app = express();
 
@@ -18,8 +20,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', indexRouter);
+app.use('/areas', areasRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
