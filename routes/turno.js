ad6b6f7bf29875974ca0a8a048ccc9a62e0f00b3
Agustin Valdez <agustinv45@gmail.com>
2022-07-03 22:33:17 -0300

    se agrega get y delete de turnos

--- routes/turno.js
+++ routes/turno.js
@@ -1,7 +1,9 @@
 const express=require('express')
 const router=express.Router()
 
-const{crearTurnos}=require('../controllers/turnos')
+const{crearTurnos, getTurnos, deleteTurnos}=require('../controllers/turnos')
 
 router .post ('/', crearTurnos)
+.get('/', getTurnos)
+.delete ('/', deleteTurnos)
 module.exports=router
